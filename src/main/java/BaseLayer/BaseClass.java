85ab2062351956d45384a4c361244ceaa3198079
Sandip Jare <sandip.jare9324@gmail.com>
2024-01-14 10:57:25 +0530

    added new githubdemo project

---
+++ src/main/java/BaseLayer/BaseClass.java
@@ -0,0 +1,13 @@
+package BaseLayer;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class BaseClass {
+
+	
+	public static void intiization() {}
+	
+	WebDriver driver=new EdgeDriver();
+	
+}
---
+++ src/test/java/TestRunner/RunnerTestClass.java
@@ -0,0 +1,15 @@
+package TestRunner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+		features= {"AllFeatureFiles"},
+		glue= {"Steps"},
+		dryRun=false
+		
+		)
+
+public class RunnerTestClass extends AbstractTestNGCucumberTests {
+
+}
