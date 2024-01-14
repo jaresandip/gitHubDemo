dd3ebf3236f4b73feb369dff97d193f8ae317da9
Sandip Jare <sandip.jare9324@gmail.com>
2024-01-14 11:16:13 +0530

    added constructor and config.properties in baseclass

--- src/main/java/BaseLayer/BaseClass.java
+++ src/main/java/BaseLayer/BaseClass.java
@@ -1,11 +1,30 @@
 package BaseLayer;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.time.Duration;
+import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class BaseClass {
+
+	static FileInputStream fis;
+	static Properties prop;
+
+	public BaseClass() {
+
+		File f = new File(System.getProperty("user.dir") + "src//main//java//CofigLayer//config.properties");
+		try {
+			fis = new FileInputStream(f);
+			prop = new Properties();
+			prop.load(fis);
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+	}
 
 	public static WebDriver driver;
 
