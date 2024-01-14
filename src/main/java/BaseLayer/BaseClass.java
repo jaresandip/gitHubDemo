83ef3751ef6dc395b300c9110b263310e609edca
Sandip Jare <sandip.jare9324@gmail.com>
2024-01-14 11:05:35 +0530

    added browser code in baseclass

--- src/main/java/BaseLayer/BaseClass.java
+++ src/main/java/BaseLayer/BaseClass.java
@@ -1,13 +1,20 @@
 package BaseLayer;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class BaseClass {
 
-	
-	public static void intiization() {}
-	
-	WebDriver driver=new EdgeDriver();
-	
+	public static WebDriver driver;
+
+	public static void intiization() {
+		driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.get("https://www.facebook.com/reg");
+
+	}
+
 }
